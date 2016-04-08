f08d99e23a7ba540a166f74a0c67a67108900423
Joshua-Holzworth <joshuaholzworth@thinkbiganalytics.com>
2016-04-08 14:38:09 -0600

    Notifier PC9 task for general setup of a trigger / event script.

---
+++ Notifier/README.md
@@ -0,0 +1,10 @@
+Usage scripts:
+
+notifier/start.py
+notifier/stop.py
+notifier/health.py
+
+
+Start boots up a new notifier process given a batchID and a configuration directory
+Stop kills a process if one exists with the given batchID
+Health checks to see if a process with the given batchID is currently running.
---
+++ Notifier/notifier/health_check.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+#######
+##	Author: Joshua Holzworth
+#######
+
+import subprocess
+import sys
+import getopt
+
+
+APP_SCRIPT_NAME = 'notifier.py'
+
+def printHelp():
+	print 'health_check.py -b <batchID>'
+
+
+def main():
+	batchID = None
+	pid = -1
+	try:
+		opts, args = getopt.getopt(sys.argv[1:],"hb:")
+	except getopt.GetoptError:
+		printHelp()
+		sys.exit(0)
+	for opt, arg in opts:
+		if opt == '-h':
+			printHelp()
+			sys.exit(0)
+		elif opt in ("-b", "--batchID"):
+			batchID = arg
+
+	if batchID is not None:
+		processListCmd = 'ps aux | grep python | grep [n]otifier | grep '+batchID 
+		listProcess = subprocess.Popen(processListCmd,stdout=subprocess.PIPE,shell=True)
+		out = listProcess.communicate()[0]
+		sections = out.split()
+		if len(out):
+			pid = sections[1]
+	print pid
+	return pid
+	
+if __name__ == "__main__":
+	exit(main())
