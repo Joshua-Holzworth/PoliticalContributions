1adfb48adbfa4435f2386008ba624c91b05f5064
Holbrook <gh186017@musgh186017-812.td.teradata.com>
2016-04-25 15:40:24 -0600

    PYTHONPATH has top level of project
    PYTHONPATH imports the top level of the project instead of src and test separately
    This is so we don't have to worry about name collisions between src and test
    
    Imports of these will now look like "import src.<module>" and "import test.<module>"
    as opposed to just "import <module"

--- install.sh
+++ install.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-printf "PYTHONPATH=\${PYTHONPATH}:$DIR/src\n" >> ~/.profile
-printf "PYTHONPATH=\${PYTHONPATH}:$DIR/test\n" >> ~/.profile
+printf "PYTHONPATH=\${PYTHONPATH}:$DIR\n" >> ~/.profile
 printf "export PYTHONPATH\n" >> ~/.profile
 source ~/.profile
 
-printf "PYTHONPATH=\${PYTHONPATH}:$DIR/src\n" >> ~/.bash_profile
-printf "PYTHONPATH=\${PYTHONPATH}:$DIR/test\n" >> ~/.bash_profile
+printf "PYTHONPATH=\${PYTHONPATH}:$DIR\n" >> ~/.bash_profile
 printf "export PYTHONPATH\n" >> ~/.bash_profile
 source ~/.bash_profile
