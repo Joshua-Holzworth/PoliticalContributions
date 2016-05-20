31ec04e703a46cdb96d0cb8fbd5022636dcddfd4
vagrant <vagrant@localhost.localdomain>
2016-05-20 13:39:59 -0400

    4HD2

--- FinishZone/src/python/interpreter.py
+++ FinishZone/src/python/interpreter.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python
+import sys
+
 from pyspark import SparkConf
 from pyspark import SparkContext
 from pyspark.sql import HiveContext
+
+try:
+    input = raw_input
+except NameError:
+    pass
 
 conf = SparkConf().setAppName('FZ SparkSQL interpreter').setMaster('local')
 conf.set('spark.eventLog.enabled', 'true')
@@ -15,8 +22,13 @@ def main():
 
     while True:
         print('Enter SQL command')
-        command = raw_input().rstrip(';')
-        sqlContext.sql(command).show(1000000, truncate=False)
+        command = input().rstrip(';')
+
+        try:
+            sqlContext.sql(command).show(1000000, truncate=False)
+        except Exception:
+           e = sys.exc_info()[1]
+           print(e)
 
 if __name__ == '__main__':
     main()
